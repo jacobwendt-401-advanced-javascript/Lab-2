23300602b2cb2f320deadd4dc48151c9bcc90360
Jacob_Wendt <jacob.wendt@yahoo.com>
2019-05-21 13:11:59 -0500

    started on the lab, made changes to classjs

--- objects/vehicle-class.js
+++ objects/vehicle-class.js
@@ -1,6 +1,35 @@
 'use strict';
 
-class Car {
+class Vehicle2 {
+    constructor(name, wheels) {
+        this.name = name;
+        this.wheels = wheels;
+    }
 }
 
-module.exports = Car;
+Vehicle2.prototype.drive = () => {
+    return 'Moving Forward';
+};
+
+Vehicle2.prototype.stop = () => {
+    return 'Stopping';
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+class Car2 extends Vehicle2 {
+    constructor(name) {
+        Vehicle2.call(this, name, 4);
+    }
+}
+
+class Motorcycle2 {
+    constructor(name) {
+Vehicle2.call(this, name, 2);
+    }
+    wheelie() {
+return 'Wheee!';
+    }
+}
+
+module.exports = {Car2, Motorcycle2};
